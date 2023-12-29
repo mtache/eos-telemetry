013d72f9776351047ef4a06687665ec9639bf806
Matthieu Tâche <mtache@arista.com>
2023-12-29 16:58:36 +0000

    tweak telegraf/influxdb config

--- influxdb/scripts/create-buckets.sh
+++ influxdb/scripts/create-buckets.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-influx bucket create --name influxdb --org arista --retention 14d
-influx bucket create --name eos-telemetry --org arista --retention 14d
+influx bucket create --name influxdb --org arista --retention 30d
+influx bucket create --name eos-telemetry --org arista --retention 30d
